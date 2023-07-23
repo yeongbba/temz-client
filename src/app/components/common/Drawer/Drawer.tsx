609e2d226919ddd246d01aec0a68a4f3eacb280d
yeongbba <yeongmo.lee@logpresso.com>
2023-07-24 02:45:13 +0900

    add drawer

--- src/app/components/common/Drawer/Drawer.tsx
+++ src/app/components/common/Drawer/Drawer.tsx
@@ -12,8 +12,7 @@ export default function Drawer({ children }: Props) {
     const { isOpen, onToggleDrawer } = useDrawer();
     return (
         <>
-            <div className={`${styles.container} ${isOpen ? styles.open : ''}`} onClick={onToggleDrawer}>
-            </div>
+            <div className={`${styles.container} ${isOpen ? styles.open : ''}`} onClick={onToggleDrawer}></div>
             <div className={`${styles.drawer} ${isOpen ? styles.open : ''}`}>
                 {children}
                 <h1>Drawer Content</h1>
